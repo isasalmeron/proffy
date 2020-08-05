8a5bb3b35e1d49d92f2b9f787a74d9542af3b8e8
Isabela Salmeron <isabela.boschi@quintoandar.com.br>
2020-08-05 20:14:14 -0300

    Integrate with server

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,32 +1,54 @@
 import React from 'react';
 
 import WhatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  };
+
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/23709852?s=460&u=487aff6baf3c712c6d0d680222cf10e837e0c1a5&v=4" alt="Isabela Salmeron"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Isabela Salmeron</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />
-        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={WhatsAppIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import LogoImg from '../../assets/images/logo.svg';
@@ -6,13 +6,22 @@ import LandingImg from '../../assets/images/landing.svg';
 import StudyIcon from '../../assets/images/icons/study.svg';
 import GiveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import api from '../../services/api';
 import './styles.css'
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      setTotalConnections(response.data.total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div id="logo-container">
+        <div className="logo-container">
           <img src={LogoImg} alt="Proffy"/>
           <h2>
             Sua plataforma de estudos online
@@ -34,7 +43,7 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo" />
+          Total de {totalConnections} conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
